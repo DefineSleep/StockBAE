53cf55bce49bff66db979f5f78a3f885afddf12d
Naiyarah Hussain <naiyarah.h@gmail.com>
2015-03-30 21:58:52 +0400

    new routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,18 +10,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-/*Route::get('/', 'HomeController@index');*/
-Route::get('/cview', 'ClientController@index');
-Route::get('/cedit', 'ClientController@edit');
-Route::get('/cdelete', 'ClientController@delete');
-Route::get('/cadd', 'ClientController@add');
+/*Route::get('/', 'HomeController@index');
+Route::get('/', 'WelcomeController@index');*/
 Route::get('/', 'ClientController@index');
-Route::get('/fview', 'FaController@index');
-Route::get('/fedit', 'FaController@edit');
-Route::get('/fdelete', 'FaController@delete');
-Route::get('/fadd', 'FaController@add');
+Route::get('client', 'ClientController@index');
+Route::get('client/add', 'ClientController@add');
+Route::get('client/edit', 'ClientController@edit');
+Route::get('client/delete', 'ClientController@delete');
 
-Route::get('home', 'HomeController@index');
+Route::get('fa', 'FaController@index');
+Route::get('fa/add', 'FaController@add');
+Route::get('fa/edit', 'FaController@edit');
+Route::get('fa/delete', 'FaController@delete');
+
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
