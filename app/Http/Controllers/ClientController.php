c1cd7fe9113f4308991d5bae1d29e43a5bcfc208
Nigel <nigel566@hotmail.com>
2015-03-30 10:56:40 +0400

    Updated FA List table code

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,71 @@
+<?php namespace App\Http\Controllers;
+
+
+use App\Client;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\User;
+use Illuminate\Http\FormC;
+
+class ClientController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+
+        $clients = Client::all();
+        return \View::make('clientlst')->with('clients',$clients);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function add()
+    {
+        $clients = Client::all()->first()->get();
+
+        $client1 = Client::updateOrCreate(['c_id' => 'c24','password' => 'letitbe','name' => 'James Holt','email' => 'diehoping@gmail.com','address' => '675R Sheikh Zayed Road ','dob' => '1967-11-04','account_no' => '11122332','ni_no' => '837282824','contact_no' => '0563439683','ni_no' => '012345678','networth' => '10000000']);
+        return \View::make('index')->with('clients',$clients);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function edit()
+    {
+        $clients = CLient::all();
+        $client1 = Client::where('c_id','gh7')->update(['contact_no'=>'056837294']);
+        $client1 = Client::where('c_id','gh7')->update(['account_no'=>'383226677']);
+        $client1 = Client::where('c_id','gh7')->update(['email'=>'wth@gmail.com']);
+        return \View::make('index')->with('clients',$clients);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function delete()
+    {
+        $clients = CLient::all()->first()
+
+
+            ->get();
+        $client1 = Client::where('c_id',"c34");
+        $client1->delete();
+        return \View::make('index')->with('clients',$clients);
+    }
+
+
+
+}
---
+++ resources/views/Transactions.blade.php
@@ -0,0 +1,41 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transactions extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'transactions';
+    protected $created_at = 'false';
+    protected $updated_at = 'false';
+
+
+
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['trans_id','trans_type','date','time', 'symbol','quantity','meeting_no'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    public function stocks()
+    {
+        return $this->belongsTo('App\Stocks');
+    }
+
+    public function meetings()
+    {
+        return $this->belongsTo('App\Meeting');
+    }
+}
